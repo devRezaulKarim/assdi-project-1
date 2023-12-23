82eea875fe4340ccc4cd1b0748451fb7863af1d2
Md. Rezaul Karim <96794203+devRezaulKarim@users.noreply.github.com>
2023-12-23 19:38:26 +0600

    Card styling

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable react/prop-types */
-import { MdAddShoppingCart } from "react-icons/md";
-import { FaInfoCircle } from "react-icons/fa";
 import { useState } from "react";
+import {
+  MdOutlineAddShoppingCart,
+  MdOutlineRemoveShoppingCart,
+  MdShoppingCartCheckout,
+} from "react-icons/md";
 
 export default function Product({ product }) {
   const { price, category, thumbnail, title, description } = product;
+  const [isAdded, setIsAdded] = useState(false);
+  const [isMouseIn, setIsMouseIn] = useState(false);
 
   return (
     <>
@@ -13,9 +18,23 @@ export default function Product({ product }) {
           <img src={thumbnail} alt={title} />
         </div>
         <div className="productInfo">
-          <h4>{title}</h4>
-          <h6>{category}</h6>
-          <h5>{price}</h5>
+          <div className={`cartBtns ${isAdded && "true"}`}>
+            <div className="addToCart">
+              <div className="cardTitle">
+                <h4>{title.slice(0, 20)}</h4>
+                <h5> ${price}</h5>
+              </div>
+              <div onClick={() => setIsAdded(true)} className="addBtn">
+                <MdOutlineAddShoppingCart />
+              </div>
+            </div>
+            <div className="removeFromCart">
+              <div onClick={() => setIsAdded(false)} className="removeBtn">
+                <MdOutlineRemoveShoppingCart />
+              </div>
+              <h4 className="addedText">Added to cart</h4>
+            </div>
+          </div>
         </div>
       </div>
     </>
